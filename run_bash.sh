33f1ca5d11d4338be46a463374434c01cd9045d2
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-02-26 11:39:58 +0100

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -4,7 +4,7 @@ echo $HOME
 
 export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 # for seed in $(seq 4 5); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/two_seeds/run_1/FPAP_200k-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --seed=$seed --layers=3; done
-for seed in $(seq 0 1); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_new_results/FSLIDE200k-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchSlide-v1 --num_timesteps=200000 --seed=$seed; done
+for seed in $(seq 0 1); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_new_results/FSLIDE200k/trial_logdir-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchSlide-v1 --num_timesteps=200000 --seed=$seed; done
 
 # python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --log_path=.log/tensorboard_test/actor_critic_loss/run4
 
