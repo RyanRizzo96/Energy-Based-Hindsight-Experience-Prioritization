f6e2a86032d78706ba82138aae5f0355cf011dfa
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-02-27 14:21:17 +0100

    Fetch Slide 400k seeds 5

--- run_bash.sh
+++ run_bash.sh
@@ -4,7 +4,7 @@ echo $HOME
 
 export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
-for seed in $(seq 0 4); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/4_seeds/log/FetchPush-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPush-v1  --num_timesteps=400000 --seed=$seed --layers=3 --save_path=/home/ubuntu/RL_baselines/.log/4_seeds/plocicies/FetchPush-$seed; done
+for seed in $(seq 0 4); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/4_seeds/log/FetchSlide-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchSlide-v1  --num_timesteps=400000 --seed=$seed --layers=3 --save_path=/home/ubuntu/RL_baselines/.log/4_seeds/plocicies/FetchSlide-$seed; done
 # for seed in $(seq 2 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_new_results/FPush_200k/trial_logdir1-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=100000 --seed=$seed; done
 
 # python3 -m baselines.run --alg=her --env=HandManipulateEggFull-v0 --num_timesteps=500000
