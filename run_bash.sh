1883f3cd99b6cc166a253103f48e4a15f6c99131
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-02-27 21:03:50 +0100

    Fpush 100k 2 seeds

--- run_bash.sh
+++ run_bash.sh
@@ -10,3 +10,4 @@ for seed in $(seq 0 1); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritizatio
 # python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000
 
 # mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --save_path=EBP_policies/FetchPAP200k/trial_1-seed4 --seed=4
+
