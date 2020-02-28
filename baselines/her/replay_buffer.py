2aaf1b77c461840fb0a8133ee3167ce4daa97849
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-02-28 21:57:08 +0100

    Update replay_buffer.py

--- baselines/her/replay_buffer.py
+++ baselines/her/replay_buffer.py
@@ -224,25 +224,25 @@ class ReplayBufferEnergy:
                 diff = np.diff(buffers['ag'], axis=1)  # difference between previous ag and new ag
                
                 dg = buffers['g'][0][0]     # We only need one desired goal for comparison
-                print("dg", dg)
+                # print("dg", dg)
     
                 # Obtaining first and last achieved goals
                 first_ag = buffers['ag'][0][0]               
-                print("first_ag", first_ag)
+                # print("first_ag", first_ag)
                 last_ag = buffers['ag'][0][49] 
-                print("last_ag", last_ag)
+                # print("last_ag", last_ag)
 
                 diff_first_ag = first_ag - dg   # Subtracting first achieved goal by desired goal
                 diff_first_ag = np.power(diff_first_ag, 2)
                 diff_last_ag = last_ag - dg
                 diff_last_ag = np.power(diff_last_ag, 2)
-                print("diff_0", diff_first_ag)
-                print("diff_49", diff_last_ag)
+                # print("diff_0", diff_first_ag)
+                # print("diff_49", diff_last_ag)
                 
                 total_diff_start = np.sum(diff_first_ag)
                 total_diff_end = np.sum(diff_last_ag)
-                print("total_diff_start", total_diff_start)
-                print("total_diff_end", total_diff_end)
+                # print("total_diff_start", total_diff_start)
+                # print("total_diff_end", total_diff_end)
 
                 # normalized_diff_start = diff_first_ag / np.sqrt(np.sum(diff_first_ag**2))
                 # normalized_diff_end = diff_last_ag / np.sqrt(np.sum(diff_last_ag ** 2))
