b921990fa6f444f9509f6db7e621cfa6494c5948
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-03-04 21:42:03 +0100

    testing FetchReach

--- run_bash.sh
+++ run_bash.sh
@@ -8,12 +8,14 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # for seed in $(seq 2 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method1/temp0.65-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 # for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.65/run2-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
-for seed in $(seq 0 1); do \
-OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchSlide/Method2/temp0.65/trial-$seed mpirun -np 16 \
-python3 -m baselines.run --alg=her --env=FetchSlide-v1 --num_timesteps=200000 --save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchSlide/Method2/temp0.65/trial-$seed --seed=$seed; done
+#for seed in $(seq 0 4); do \
+#OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/Method2/temp0.5/trial-$seed mpirun -np 16 \
+#python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=100000 --save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/Method2/temp0.5/trial-$seed --seed=$seed; done
 
 
- #python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=5000
+for seed in $(seq 0 9); do OPENAI_LOGDIR=/Users/ryanr/B.Eng/MCAST_Degree_4/Thesis/code/gym/RL_EBP/EBP_mod/FetchReach/Method1/temp0.5/trial-$seed \
+python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 \
+--save_path=/Users/ryanr/B.Eng/MCAST_Degree_4/Thesis/code/gym/RL_EBP/EBP_mod_policies/FetchReach/Method1/temp0.5/trial-$seed --seed=$seed; done
 
 #mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --save_path=EBP_policies/FetchPAP200k/trial_1-seed4 --seed=4
 
