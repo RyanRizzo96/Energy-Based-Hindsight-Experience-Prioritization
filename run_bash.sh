fd831d2dc41c3183171e992f2ffa66f1941f1a40
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-03-04 12:24:03 +0100

    Fetch-Push/Method2/temp1

--- run_bash.sh
+++ run_bash.sh
@@ -8,7 +8,7 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # for seed in $(seq 2 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method1/temp0.65-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 # for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.65/run2-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
-for seed in $(seq 0 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.5/trial-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
+for seed in $(seq 0 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-Push/Method2/temp1/trial-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=100000 --seed=$seed; done
 
  #python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=5000
 
