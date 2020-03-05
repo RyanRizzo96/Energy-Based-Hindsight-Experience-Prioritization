ebf09223caad2ad064c4168f481c437d7b0955a7
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-03-05 13:50:01 +0100

    FPUSH EBP 100k x 5

--- baselines/her/her_sampler.py
+++ baselines/her/her_sampler.py
@@ -117,26 +117,26 @@ def make_sample_her_transitions_energy(replay_strategy, replay_k, reward_fun):
                 # print("Inside her samlper")
                 # print(energy_direction, sample_count, cycle_count-1)
 
-                if logger:  # Every cycle
-                    if energy_direction[cycle_count-1] == 1:
-                        # print("en traj before", episode_batch['e'])
-                        # print(energy_direction, sample_count, cycle_count - 1)
-                        # print("We must increase probability of sampling at index [",
-                        #       cycle_count-1, "]", energy_trajectory[cycle_count-1])
-                        max_energy = np.max(energy_trajectory)
-                        if energy_trajectory[cycle_count - 1] < max_energy:
-
-                            # METHOD 1
-                            # energy_trajectory[cycle_count - 1] = max_energy
-
-                            # METHOD 2
-                            increased_avg_energy = (max_energy + energy_trajectory[cycle_count - 1]) / 2
-                            # print("max", max_energy)
-                            energy_trajectory[cycle_count - 1] = increased_avg_energy
-                            # print("Now probability is of sampling at index [", cycle_count-1, "]",
-                            #       energy_trajectory[cycle_count-1])
-                        # else:
-                        #     print("Already at max energy")
+                # if logger:  # Every cycle
+                #     if energy_direction[cycle_count-1] == 1:
+                #         # print("en traj before", episode_batch['e'])
+                #         # print(energy_direction, sample_count, cycle_count - 1)
+                #         # print("We must increase probability of sampling at index [",
+                #         #       cycle_count-1, "]", energy_trajectory[cycle_count-1])
+                #         max_energy = np.max(energy_trajectory)
+                #         if energy_trajectory[cycle_count - 1] < max_energy:
+                #
+                #             # METHOD 1
+                #             # energy_trajectory[cycle_count - 1] = max_energy
+                #
+                #             # METHOD 2
+                #             increased_avg_energy = (max_energy + energy_trajectory[cycle_count - 1]) / 2
+                #             # print("max", max_energy)
+                #             energy_trajectory[cycle_count - 1] = increased_avg_energy
+                #             # print("Now probability is of sampling at index [", cycle_count-1, "]",
+                #             #       energy_trajectory[cycle_count-1])
+                #         # else:
+                #         #     print("Already at max energy")
             else:
                 energy_trajectory = episode_batch['p']
             p_trajectory = np.power(energy_trajectory, 1 / (1 + 1e-2))
--- run_bash.sh
+++ run_bash.sh
@@ -10,8 +10,8 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 # for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.65/run2-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 
 for seed in $(seq 0 4); do \
-OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/Method2/temp1/trial-$seed mpirun -np 16 \
-python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=50000 --save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_policy/FetchPush/Method2/temp1/trial-$seed --seed=$seed; done
+OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/EBP/temp1/trial-$seed mpirun -np 16 \
+python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=50000 --save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_policy/FetchPush/EBP/temp1/trial-$seed --seed=$seed; done
 
 
 #for seed in $(seq 0 9); do OPENAI_LOGDIR=/Users/ryanr/B.Eng/MCAST_Degree_4/Thesis/code/gym/RL_EBP/EBP_mod/FetchReach/EBP/temp0.5/trial-$seed \
