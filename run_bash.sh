a31fae5484107d8a01bb3ee9c3ba3fd7334e052a
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-03-05 11:15:03 +0100

    Fpush 100k temp1

--- run_bash.sh
+++ run_bash.sh
@@ -10,8 +10,8 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 # for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.65/run2-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 
 for seed in $(seq 0 4); do \
-OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/Method2/temp0.5/trial-$seed mpirun -np 16 \
-python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=100000 --save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_policy/FetchPush/Method2/temp0.5/trial-$seed --seed=$seed; done
+OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/Method2/temp1/trial-$seed mpirun -np 16 \
+python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=50000 --save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_policy/FetchPush/Method2/temp1/trial-$seed --seed=$seed; done
 
 
 #for seed in $(seq 0 9); do OPENAI_LOGDIR=/Users/ryanr/B.Eng/MCAST_Degree_4/Thesis/code/gym/RL_EBP/EBP_mod/FetchReach/EBP/temp0.5/trial-$seed \
@@ -20,3 +20,4 @@ python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=100000 --s
 
 #mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --save_path=EBP_policies/FetchPAP200k/trial_1-seed4 --seed=4
 
+# /Users/ryanr/Desktop/MCAST_Degree_4/Results/FetchPush/DEBP/Method2/temp0.65/aggregates/testsuccess_rate-tb-temp0.65.csv/Users/ryanr/Desktop/MCAST_Degree_4/Results/FetchPush/DEBP/Method2/temp0.65/aggregates/testsuccess_rate-tb-temp0.65.csv/Users/ryanr/Desktop/MCAST_Degree_4/Results/FetchPush/DEBP/Method2/temp0.65/aggregates/testsuccess_rate-tb-temp0.65.csv
