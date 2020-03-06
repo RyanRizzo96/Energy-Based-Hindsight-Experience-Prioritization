c6df13ef6bfa2d9a6337a8f796599404f41628c9
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-03-06 15:50:53 +0100

    FSLIDE M3 T1

--- run_bash.sh
+++ run_bash.sh
@@ -9,10 +9,10 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 # for seed in $(seq 2 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method1/temp0.65-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 # for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.65/run2-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 
-for seed in $(seq 0 4); do \
-OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPickAndPlace/Method3/temp0.65/trial-$seed mpirun -np 16 \
-python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 \
---save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_policy/FetchPickAndPlace/Method3/temp0.65/trial-$seed --seed=$seed; done
+for seed in $(seq 0 2); do \
+OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchSlide/Method3/temp1/trial-$seed mpirun -np 16 \
+python3 -m baselines.run --alg=her --env=FetchSlide-v1 --num_timesteps=200000 \
+--save_path=/home/ubuntu/Energy-Based-Prioritization/EBP_policy/FetchSlide/Method3/temp1/trial-$seed --seed=$seed; done
 
 #for seed in $(seq 0 4); do \
 #OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/FetchPush/Method3/temp0.7/trial-$seed mpirun -np 16 \
