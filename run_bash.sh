55fe01199c4194ea380c203f3fb28d3140ffa1cb
Ubuntu <ubuntu@ip-172-31-42-121.eu-central-1.compute.internal>
2020-06-15 09:29:52 +0000

    Thesis FINAL DEMO

--- run_bash.sh
+++ run_bash.sh
@@ -9,7 +9,7 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 # for seed in $(seq 2 4); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method1/temp0.65-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 # for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/Energy-Based-Prioritization/EBP_mod/Fetch-PickAndPlace/Method2/temp0.65/run2-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=100000 --seed=$seed; done
 
-python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=1000 --save_path=~./models/thesis_demo/run4
+python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=2000 --save_path=~./models/thesis_demo_model/run4
 
 # python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=0 --load_path=/models/thesis_demo/run4 --play
 
